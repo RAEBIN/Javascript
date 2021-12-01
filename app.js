d22225fa65126f0866d4cd4bd315dda0e1073b74
RAEBIN <leejieun.sw@gmail.com>
2021-12-01 09:24:44 +0900

    1130

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
- //html은 css와 js 의 중간 역할 (html 접근하고 읽어옴/변경가능). 얘가 브라우저에서 실행되어 다른애들도 실행시켜줌
- 
-//JS는 HTML에 접근하고 읽어올 수 있다. 모든것들은 document(=web site/html)로 부터 시작됨.
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-const title = document.querySelector(".hello h1");//css selector 수가 많이 존재하면 첫번째 1개만 가져옴
+const HIDDEN_CLASSNAME = "hidden";
 
-//title.innerText = "hello";
 
-//console.log(title);
-
-function handleTitleClick(){
-    //console.log("titile was clicked!");
-    title.style.color = "blue";
+function onLoginSubmit(event){
+    event.preventDefault();//새로고침 막음
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    //console.log(username);
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    
 }
 
-function handleMouseenter(){
-    title.innerText= "Mouse is here!";
 
-}
-
-function handleMouseleave(){
-    title.innerText= "Mouse is gone!";
-}
-
-//function을 js에 넘겨줘서 user가 click할때 function이 실행되도록 함
-title.addEventListener("click",handleTitleClick);
-title.addEventListener("mouseenter",handleMouseenter);
-title.addEventListener("mouseleave",handleMouseleave);
+loginForm.addEventListener("submit",onLoginSubmit);
